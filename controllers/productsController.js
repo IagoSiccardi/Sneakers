8c2984cf4ba80f707dc9888b86659e4874298dcb
IagoSiccardi <iago.siccardi@gmail.com>
2022-05-14 00:19:18 -0300

    CRUD  Letter U

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,15 +1,59 @@
-const req = require('express/lib/request')
 const products = require('../data/products.json')
-
-
+const fs = require ('fs')
+const path = require('path')
 
 module.exports = {
 
 
-    products:(req,res) => {
+    products: (req,res) => {
+
+        const products =  JSON.parse(fs.readFileSync(path.resolve(__dirname, '..','data','products.json')))
+        
+
         res.render('./products/products',{
             products
         })
+    },
+
+    edit:(req,res) => {
+        
+        const {id} = req.params
+
+        const product = products.find(product => product.id === +id)
+
+        res.render('./products/edit',{
+            product,
+            products
+        })
+    },
+
+    update:(req,res) => {
+
+        const {id} = req.params
+        const {name,price,description} = req.body
+
+        const productsEdit = products.map(product => {
+
+            if (product.id === +id) {
+
+                let productEdit = {
+    
+                    ...product,
+                    name: name.trim(),
+                    price : +price,
+                    description: description.trim()
+                }
+
+                return productEdit
+            }
+
+            return product
+
+        })
+
+        fs.writeFileSync(path.resolve(__dirname,'..','data','products.json'),JSON.stringify(productsEdit,null,3),'utf-8')
+
+        res.redirect('/products')
     },
 
     detail: (req,res) => {
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const {detail, products,search} = require('../controllers/productsController')
+const {detail, products,search, edit, update} = require('../controllers/productsController')
 
 
 router.get('/',products)
 router.get('/detail/:id',detail)
 router.get('/result',search)
+
+router.get('/edit/:id', edit)
+router.put('/update/:id', update)
 
 
 
